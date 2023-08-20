239b7ff6739ec66d792b09c99437ca0f498c514f
wendryo <wendryo.sales@gmail.com>
2023-08-20 10:19:50 -0300

    feat: bodyschema done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,53 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { MongoClient } from "mongodb";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// db
+const client = new MongoClient("mongodb://localhost:27017");
 
+await client.connect();
+const db = client.db("elysia");
+const collection = db.collection("pessoas");
+
+const app = new Elysia();
+
+const bodySchema = t.Object(
+  {
+    apelido: t.Union([t.String({ maxLength: 32 }), t.Null()], {
+      error: "Apelido inválido",
+    }),
+    nome: t.Union([t.String({ maxLength: 100 }), t.Null()], {
+      error: "Nome inválido",
+    }),
+    nascimento: t.RegExp(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/, {
+      error: "Nascimento inválido",
+    }),
+    stacks: t.Union([t.Array(t.String({ maxLength: 32 })), t.Null()], {
+      error: "Stacks inválidas",
+    }),
+  },
+  {
+    error: "Corpo inválido",
+  },
+);
+
+app.post(
+  "/pessoas",
+  ({ body, set }) => {
+    set.status = 201;
+    return body;
+  },
+  {
+    beforeHandle: ({ body, set }) => {
+      if (!body.nome || !body.apelido) {
+        set.status = 422;
+        return { error: "Nome e apelido são obrigatórios" };
+      }
+    },
+    body: bodySchema,
+  },
+);
+
+app.listen(3000);
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
